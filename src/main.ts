16358befd27250a3f05d217b6c9d00e46c759376
lnieblas <lnieblas@kreitech.com.uy>
2024-01-24 01:20:20 -0500

    ADD ResponseTransformInterceptor to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import {ConfigService} from "@nestjs/config";
 import {ValidationPipe} from "@nestjs/common";
 import {Logger} from "nestjs-pino";
+import {ResponseTransformInterceptor} from "./shared/interceptors/response-transform.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,7 @@ async function bootstrap() {
   app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   // app.useGlobalFilters(new MongooseExceptionFilter(), new HttpExceptionFilter(httpAdapter));
-  // app.useGlobalInterceptors(new ResponseTransformInterceptor());
+  app.useGlobalInterceptors(new ResponseTransformInterceptor());
   app.useLogger(app.get(Logger));
 
   await app.listen(port);
